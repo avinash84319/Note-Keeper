ba59ed4e604558237c69dcdb3122ff3b4404d878
avinash84319 <avichinnu22@gmail.com>
2022-09-21 15:06:15 +0530

    Update notes.js

--- src/notes.js
+++ src/notes.js
@@ -1,8 +1,8 @@
 const notes=[
     {
         id:1,
-        title : "ddefault note",
-        content : "hi i am author avinash reddy c took help of angella yu in creating this react app"
+        title : "-Avinash Reddy c",
+        content : "welcome to my new react app(created with angella yu 's mentroing"
     }
 ]
 
